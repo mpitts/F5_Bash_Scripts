9ec21bb782f478cc4a64dfd04762b2ff6c4e9ae6
mpitts <mpitts7@gmail.com>
2016-11-09 20:56:32 -0600

    Added prompt
    Added prompt to enter the cipher you’re searching for.

--- cipher_change.sh
+++ cipher_change.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cipher=":RC4-SHA"
+read -r -p "Enter the cipher you are looking for (example: RC4-SHA) [ENTER]: " cipher
+echo " "
 
-for x in `tmsh list ltm profile client-ssl ciphers | grep -B 1 $cipher | grep "profile" | awk -F" " '{ print $4 }'`
-do 
-	tmsh list ltm profile client-ssl defaults-from | grep -A 1 $x
+for profile in `tmsh list ltm profile client-ssl ciphers | grep -B 1 $cipher | grep "profile" | awk -F" " '{ print $4 }'`
+do
+	tmsh list ltm profile client-ssl defaults-from | grep -A 1 $profile
 done
--- cipher_check.sh
+++ cipher_check.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-cipher=":RC4-SHA"
+read -r -p "Enter the cipher you are looking for (example: RC4-SHA) [ENTER]: " cipher
+echo " "
 
-for x in `tmsh list ltm profile client-ssl ciphers | grep -B 1 $cipher | grep "profile" | awk -F" " '{ print $4 }'`
+for profile in `tmsh list ltm profile client-ssl ciphers | grep -B 1 $cipher | grep "profile" | awk -F" " '{ print $4 }'`
 do
-	for y in `echo y | tmsh list ltm virtual profiles | grep -B 10 $x | grep "virtual" | awk -F" " '{ print $3 }'`
+	for vip in `echo y | tmsh list ltm virtual profiles | grep -B 10 $profile | grep "virtual" | awk -F" " '{ print $3 }'`
 	do
-		state=$(tmsh show ltm virtual $y | grep "Availability" | awk -F" " '{ print $3 }')
-		echo $y "     " $state
+		state=$(tmsh show ltm virtual $vip | grep "Availability" | awk -F" " '{ print $3 }')
+		echo "Virtual Server:" $vip "-- Client-SSL Profile:" $profile "-- Availability:" $state
 	done
 done
